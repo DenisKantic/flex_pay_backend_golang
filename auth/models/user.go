e14852160eef06f58384f3aa90540fcf8b97be5d
DenisKantic <denis.kantic18@gmail.com>
2025-01-07 22:40:38 +0100

    created functionality to check if email exist and username and some small tweaks

--- auth/models/user.go
+++ auth/models/user.go
@@ -4,6 +4,6 @@ type User struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	CardNum  int    `json:"card_num"`
+	CardNum  string `json:"card_num"`
 	ValidTo  string `json:"valid_to"`
 }
