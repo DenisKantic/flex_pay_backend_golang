9b2d519b5326e11996b744758ca9511275f33353
DenisKantic <denis.kantic18@gmail.com>
2025-01-11 10:01:19 +0100

    added cors and removed activation code property from db and golang

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"paypal_clone_project/auth"
 )
 
 func main() {
-	// Run Gin in the correct mode
 	gin.SetMode(gin.ReleaseMode)
 
-	//if err := godotenv.Load(".env"); err != nil {
-	//	log.Fatal("Error loading .env file")
-	//}
+	r := gin.Default()
 
-	r := gin.Default() // Create a new Gin router
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
-	r.POST("/register", auth.Register) // register user account
-	r.POST("/login", auth.Login)       // login in user account
+	r.POST("/register", auth.Register)
+	r.POST("/login", auth.Login)
 	r.GET("/protected", auth.VerifyJWT, func(c *gin.Context) {
 		email, _ := c.Get("email")
 		c.JSON(200, gin.H{
@@ -25,7 +29,6 @@ func main() {
 		})
 	})
 
-	// Start the server on port 8080
 	err := r.Run(":8080")
 	if err != nil {
 		return
